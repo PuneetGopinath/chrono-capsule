398b32fb8fd73d2caf70e5a47dd64203fd89e761
PuneetGopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-06-28 12:59:58 +0530

    feat: implement decryption for capsules and enhance mailer functionality

--- src/server/utils/mailer.js
+++ src/server/utils/mailer.js
@@ -1,25 +1,56 @@
 const nodemailer = require("nodemailer");
 
+const decrypt = require("./decrypt");
+
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     secure: process.env.SMTP_SECURE === "true", // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
+        pass: process.env.SMTP_PASS || ""
     }
 });
 
 const mailer = async (capsules) => {
+    // Upgrade to use EJS templates later
+    const html = (name, msg) => (`<!DOCTYPE html>
+<html>
+    <body style="font-family:Arial, sans-serif; line-height:1.6; color:#333;">
+        <div style="max-width:600px; margin:auto; padding:20px; border:1px solid #ccc; box-sizing: border-box">
+            <h2 style="color: #4a90e2;">📬 <span style="display:none;">New Capsule:</span> A Chrono Capsule Has Arrived!</h2>
+            <p>Hello <strong>${name}</strong>,</p>
+            <p>🚀 The future has arrived...! A capsule addressed to you has just been unlocked—feel free to explore the message and any attached media.</p>
+            <blockquote style="font-size: 1.1em; color: #444; background: #f9f9f9; padding: 10px 15px; border-left: 4px solid #4a90e2;">
+                ${msg}
+            </blockquote>
+            <p style="font-size:0.9em; color:#666;">Thank you for using Chrono Capsule. Keep traveling through time responsibly ⏳</p>
+            <a href="" style="display:inline-block; padding:10px 15px;">Visit Chrono Capsule</a>
+        </div>
+    </body>
+</html>`);
+
+    const text = (name, msg) => (`📬 A Chrono Capsule Has Arrived!
+Hello ${name},\n
+🚀 The future has arrived...! A capsule addressed to you has just been unlocked—feel free to explore the message and any attached media.
+
+>> ${msg}\n
+Thank you for using Chrono Capsule. Keep traveling through time responsibly ⏳`);
+
     try {
         const sent = [];
-        for (const c of capsules) {
+        for (const _c of capsules) {
+            let c;
+            if (_c.isEncrypted) {
+                c = decrypt(_c, process.env.ENCRYPTION_KEY);
+            } else 
+                c = _c;
             const info = await transporter.sendMail({
                 from: `"Chrono Capsule" <${process.env.SMTP_USER}>`, // sender address
                 to: c.recipient.email, // list of receivers
                 subject: "Your Chrono Capsule is Unlocked!", // Subject line
-                text: `Hello ${c.recipient.name},\n\nYour capsule with the message "${c.message}" has been unlocked! You can now view it.\n\nBest regards,\nChrono Capsule Team`,
-                html: `<p>Hello${c.recipient.name},</p><p>Your capsule with the message "<strong>${c.message}</strong>" has been unlocked! You can now view it.</p><p>Best regards,<br>Chrono Capsule Team</p>`,
+                text: text(c.recipient.name, c.message),
+                html: html(c.recipient.name, c.message),
                 attachments: c.media ? c.media : []
             });
             //TODO: Optimize for scalability
@@ -31,6 +62,7 @@ const mailer = async (capsules) => {
         return sent;
     } catch (err) {
         console.log("❌ Error sending emails:", err.message);
+        return false;
     }
 };
 
--- src/server/utils/scheduler.js
+++ src/server/utils/scheduler.js
@@ -2,6 +2,7 @@ const { Capsule } = require("../models");
 const mailer = require("./mailer");
 
 const unlockCapsules = async () => {
+    if (process.env.DEBUG) console.log("🔓 Checking for capsules to be unlocked...");
     try {
         const now = new Date();
         const unlocked = await Capsule.find({
@@ -10,13 +11,17 @@ const unlockCapsules = async () => {
         });
         if (unlocked.length > 0) {
             console.log(`Unlocking ${unlocked.length} capsules...`);
-            if (process.env.DEBUG) console.log("Recipients:", unlocked.map(c => `${c.recipient.name},${c.recipient.email}`).join(", "));
-            await Capsule.updateMany(
-                { _id: { $in: unlocked.map(c => c._id) } },
-                { $set: { opened: true } }
-            );
-            await mailer(unlocked);
-            console.log("Capsules unlocked and emails sent.");
+            if (process.env.DEBUG) console.log("Recipients:", unlocked.map(c => `${c.recipient.name}=>${c.recipient.email}`).join(", "));
+            const success = await mailer(unlocked);
+            if (success) {
+                await Capsule.updateMany(
+                    { _id: { $in: unlocked.map(c => c._id) } },
+                    { $set: { opened: true } }
+                );
+                console.log("Capsules unlocked and emails sent.");
+            } else {
+                console.log("❌ Failed to send emails for unlocked capsules.");
+            }
         }
     } catch (err) {
         console.log("Error unlocking capsules:", err);
