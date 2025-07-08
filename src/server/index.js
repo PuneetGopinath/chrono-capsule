e1739181558a965d703b1f1a8a9dab2cb919355c
PuneetGopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-07-08 19:26:18 +0530

    fix: enable trust proxy for handling forwarded requests
    Express respects `X-Forwarded-For` and lets rate-limiters track users properly by their IP.

--- src/server/index.js
+++ src/server/index.js
@@ -18,6 +18,8 @@ const unlockCapsules = require("./utils/scheduler");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("trust proxy", true);
+
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
