489dbde1a97c9f6954e9e43ce138d86a2f07767e
PuneetGopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-07-15 15:12:27 +0530

    Revert "fix: replace useNavigate with window.location.href for logout redirection"
    This reverts commit f8a0599f0b4cc1160acaae4c3399039b08a72280.

--- src/client/components/LoggedIn.jsx
+++ src/client/components/LoggedIn.jsx
@@ -4,7 +4,10 @@
  * License: MIT (see LICENSE)
 */
 
+import { useNavigate } from "react-router-dom";
+
 export default function LoggedIn({ text, setLoggedIn }) {
+    const navigate = useNavigate();
     
     return (
         <main>
@@ -13,7 +16,7 @@ export default function LoggedIn({ text, setLoggedIn }) {
             <button className="auth-button" onClick={() => {
                 localStorage.removeItem("token");
                 setLoggedIn(false);
-                window.location.href = "/login"; // Redirect to login page
+                navigate("/login"); // Redirect to login page
                 // Since we are modifying localStorage, we have to reload the page to reflect the changes
             }}>Logout</button>
         </main>
