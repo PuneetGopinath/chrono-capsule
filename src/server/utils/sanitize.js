827dc622bad8a994ad47a4f831d0752549aeb7f2
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-07-29 21:25:16 +0530

    fix: update replace method with replaceAll for username sanitization, do not trim for password

--- src/server/utils/sanitize.js
+++ src/server/utils/sanitize.js
@@ -1,12 +1,15 @@
 export default function sanitize(input, type) {
     switch (type) {
         case "username":
-            return input.replace(/[^A-Za-z0-9\._\-@]/, "").trim();
+            return input.replaceAll(/[^A-Za-z0-9\._\-@]/, "").trim();
         
         case "email":
             return input.trim().toLowerCase(); // Many email providers are case-insensitive
         
         case "password":
-            return input.trim();
+            return input;
+        default:
+            console.log(`Unknown sanitize type: ${type}`);
+            return input;
     }
 };
