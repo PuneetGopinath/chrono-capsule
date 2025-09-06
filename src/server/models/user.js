12ea3dabf2584cbbc284dd52326bf4709c84c486
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-09-06 10:48:03 +0530

    feat: add googleId field to User schema

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -43,6 +43,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         enum: ["local", "google"],
         default: "local"
+    },
+    googleId: {
+        type: String,
+        default: null
     }
 });
 
