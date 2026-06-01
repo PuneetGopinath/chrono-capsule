72a56d96658510b85609ff7a3ab229b09f68388d
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-01 21:14:44 +0530

    deps: add react-router, update react and react-dom, remove react-router-dom and fix basic vulnerabilities
    also update files as per changes of dependencies

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -5,7 +5,7 @@
 */
 
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import "../home.css";
 
 export default function Home() {
