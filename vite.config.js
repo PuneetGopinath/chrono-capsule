c8d4e4afccc49070529ab65aac5d008a600e2224
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-05 19:19:26 +0530

    fix: add envDir in vite config to specify the location of the .env file

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import path from "path";
+
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,6 +8,7 @@ export default defineConfig(({ mode }) => {
 
     return {
         root: "./src/client",
+        envDir: path.resolve(__dirname),
         plugins: [react()],
         server: {
             proxy: {
