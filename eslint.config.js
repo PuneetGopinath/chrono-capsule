e99e0eb6fb717031a9bbdc57d35eed47e2b2d784
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2026-06-30 12:01:54 +0530

    chore: fix errors in eslint configuration and use @eslint/js

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,40 @@
 import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
 import globals from "globals";
 import eslintReact from "@eslint-react/eslint-plugin";
 import reactHooks from "eslint-plugin-react-hooks";
 
-export default defineConfig({
-    basePath: "./src",
-    files: ["**/*.{js,jsx}"],
-    languageOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        globals: {
-            ...globals.browser,
-            ...globals.node
-        },
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true
+export default defineConfig(
+    js.configs.recommended,
+    eslintReact.configs.recommended,
+    {
+        files: ["src/**/*.{js,jsx}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                ...globals.browser,
+                ...globals.node
+            },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                }
             }
+        },
+        plugins: {
+            "react-hooks": reactHooks
+        },
+        rules: {
+            "semi": ["error", "always"],
+            "quotes": ["error", "double"],
+            "no-unused-vars": ["warn"],
+            "no-console": ["warn"],
+
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "warn",
+
+            "@eslint-react/no-nested-component-definitions": "error"
         }
-    },
-    plugins: {
-        "react-hooks": reactHooks
-    },
-    extends: [
-        "eslint:recommended",
-        eslintReact.configs.recommended
-    ],
-    rules: {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"],
-        "no-unused-vars": ["warn"],
-        "no-console": ["warn"],
-
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-
-        "@eslint-react/naming-convention/component-name": "error",
-        "@eslint-react/no-unstable-nested-components": "error"
     }
-});
+);
